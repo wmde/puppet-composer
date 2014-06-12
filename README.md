12ab61590472019e0819ed5c3608c6910ed39b62
aude <aude.wiki@gmail.com>
2014-06-12 13:45:02 +0000

    Use composer 1.0.0-alpha8 from gerrit

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Puppet module for composer
 
-Installs composer.phar (https://getcomposer.org/) to /usr/local/bin
-so that it can be run system-wide.
+Installs composer from https://git.wikimedia.org/summary/integration%2Fcomposer.git
+to /usr/local/bin so that it can be run system-wide.
 
 # Version
 
-Composer version 7adc41d02c3536b3e19a6b906cf0c4cf6d3beb70 2014-01-20 18:10:11
+Composer version 1.0.0-alpha8
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,24 @@
-class composer() {
+class composer(
+    $base_dir
+) {
 
   package { [
       'php5-cli',
     ]: ensure => present,
   }
 
+  git::clone { 'composer':
+      ensure => latest,
+      directory => "${base_dir}/composer",
+      origin => 'https://gerrit.wikimedia.org/r/p/integration/composer.git',
+      recurse_submodules => true;
+  }
+
   file {
     "/usr/local/bin/composer":
-      ensure => present,
-      owner => 'root',
-      group => 'root',
+      ensure => link,
       mode => 0755,
-      source => 'puppet:///modules/composer/composer.phar';
+      target => "${base_dir}/composer/vendor/bin/composer";
   }
 
 }
